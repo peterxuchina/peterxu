0dd25603b4c4df439e0ebef80b6b1d5b4c80d274
xuyongjun <xuyongjun@yongche.com>
2020-07-20 18:09:12 +0800

    #

--- new.php
+++ new.php
@@ -2,11 +2,11 @@
 $a = 'abaca';
 $b = 123;
 $a = 13;
-if($a){
-		echo 3333;
-		   print($a);
-}else{
-		echo 12334;
-		   print($a);
+if ($a) {
+    echo 3333;
+    print($a);
+} else {
+    echo 12334;
+    print($a);
 }
 
